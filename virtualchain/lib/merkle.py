df3db1912a4234f9fc379e00ab532a04c5241c94
Jude Nelson <judecn@gmail.com>
2016-05-31 12:52:00 -0400

    import the minimal Pybitcoin functionality we actually need for virtualchain

--- virtualchain/lib/merkle.py
+++ virtualchain/lib/merkle.py
@@ -1,27 +1,5 @@
-##!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-     Virtualchain
-     ~~~~~
-     copyright: (c) 2014-2015 by Halfmoon Labs, Inc.
-     copyright: (c) 2016 by Blockstack.org
+from .hash import bin_double_sha256, bin_to_hex_reversed, hex_to_bin_reversed
 
-     This file is part of Virtualchain
-
-     Virtualchain is free software: you can redistribute it and/or modify
-     it under the terms of the GNU General Public License as published by
-     the Free Software Foundation, either version 3 of the License, or
-     (at your option) any later version.
-
-     Virtualchain is distributed in the hope that it will be useful,
-     but WITHOUT ANY WARRANTY; without even the implied warranty of
-     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-     GNU General Public License for more details.
-     You should have received a copy of the GNU General Public License
-     along with Virtualchain.  If not, see <http://www.gnu.org/licenses/>.
-"""
-
-from hash import bin_double_sha256, bin_to_hex_reversed, hex_to_bin_reversed
 
 def hex_to_bin_reversed_hashes(hex_hashes):
     return [hex_to_bin_reversed(h) for h in hex_hashes]
