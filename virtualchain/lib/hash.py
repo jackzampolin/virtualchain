782deb5f19773f31a612316738b2a1f4294b2f00
Jude Nelson <judecn@gmail.com>
2016-05-31 12:49:26 -0400

    (will be overwritten by merge from Pybitcoin)

---
+++ virtualchain/lib/hash.py
@@ -0,0 +1,72 @@
+##!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+     Virtualchain
+     ~~~~~
+     copyright: (c) 2014-2015 by Halfmoon Labs, Inc.
+     copyright: (c) 2016 by Blockstack.org
+
+     This file is part of Virtualchain
+
+     Virtualchain is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation, either version 3 of the License, or
+     (at your option) any later version.
+
+     Virtualchain is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+     You should have received a copy of the GNU General Public License
+     along with Virtualchain.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import hashlib
+from hashlib import sha256
+from binascii import hexlify, unhexlify
+from utilitybelt import is_hex
+
+
+def bin_sha256(bin_s):
+    return sha256(bin_s).digest()
+
+
+def bin_checksum(bin_s):
+    """ Takes in a binary string and returns a checksum. """
+    return bin_sha256(bin_sha256(bin_s))[:4]
+
+
+def bin_double_sha256(bin_s):
+    return bin_sha256(bin_sha256(bin_s))
+
+
+def bin_hash160(s, hex_format=False):
+    """ s is in hex or binary format
+    """
+    if hex_format and is_hex(s):
+        s = unhexlify(s)
+    return hashlib.new('ripemd160', bin_sha256(s)).digest()
+
+
+def hex_hash160(s, hex_format=False):
+    """ s is in hex or binary format
+    """
+    if hex_format and is_hex(s):
+        s = unhexlify(s)
+    return hexlify(bin_hash160(s))
+
+
+def reverse_hash(hash, hex_format=True):
+    """ hash is in hex or binary format
+    """
+    if not hex_format:
+        hash = hexlify(hash)
+    return "".join(reversed([hash[i:i+2] for i in range(0, len(hash), 2)]))
+
+
+def hex_to_bin_reversed(s):
+    return unhexlify(s.encode('utf8'))[::-1]
+
+
+def bin_to_hex_reversed(s):
+    return hexlify(s[::-1])
